f34ae8f824f4f7be191ad299c29ded868bdb35f7
Dammike Saman <dimmuborgir.damie@gmail.com>
2021-12-06 02:05:38 +1300

    Add dependencies

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const routes = require('./routes/routes');
+
+app.use(express.json());
+app.use('/api', routes);
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server up and running on Port: ${PORT}`));
