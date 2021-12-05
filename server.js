349d18b9285c559f721ce89d78b57659a13547bb
Dammike Saman <dimmuborgir.damie@gmail.com>
2021-12-06 12:25:30 +1300

    Add public folder to host assets like images.

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ mongoose.connect(process.env.DB_URL)
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static('public'));
 app.use('/api/feeds', feeds);
 app.use('/api/pilots', pilots);
 app.use('/api/flyingSites', flyingSites);
