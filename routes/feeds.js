641cb9982ab878e678563b722a48e3e593148f6d
Dammike Saman <dimmuborgir.damie@gmail.com>
2021-12-06 20:36:17 +1300

    Sort feeds by popularity, trending etc.

--- routes/feeds.js
+++ routes/feeds.js
@@ -8,4 +8,25 @@ router.get('/', async (req, res) => {
     res.send(feeds);
 });
 
+router.get('/search', async (req, res) => {
+    const sortBy = req.query.sortBy;
+    const feeds = await Feed.find();
+    if (sortBy) {
+        switch (sortBy) {
+            case "popularity":
+                return res.send(feeds.sort((a, b) => parseInt(b.viewCount) - parseInt(a.viewCount)));
+                break;
+            case "latest":
+                return res.send(feeds.sort((a, b) => new Date(b.date) - new Date(a.date)));
+                break;
+            case "competition":
+                return res.send(feeds.sort((a, b) => new Date(b.date) - new Date(a.date)));
+                break;
+            default:
+                return res.status(400).send('bad search query!');
+        }
+    }
+    res.status(400).send('Incomplete URL request');
+});
+
 module.exports = router;
